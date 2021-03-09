be66f23b8c982681f513a8e957848e078c1ee395
Elise Hodnefjell <EliseHodnefjell@Elise-H.local>
2021-03-09 08:45:40 +0100

    Tirsdag nr. 2

--- Navn.java
+++ Navn.java
@@ -2,7 +2,7 @@ public class Navn
 {
    public static void main(String args[])
    {
-       System.out.println("Elise Hodnefjell");
+       System.out.println("Elise Hodnefjell 09.03.21 ");
 }
 }
 
